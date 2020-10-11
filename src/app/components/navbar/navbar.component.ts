b074a0a2fe1f2e01fef20094d0f0895dcf0a8546
Ashwin <ashwingairola999@gmail.com>
2020-10-11 12:58:04 +0530

    Completed mobile navbar functionality

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+	showMobileMenu = false;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	onClickBurger(): void {
+		this.showMobileMenu = true;
+	}
+
+	onClickClose(): void {
+		this.showMobileMenu = false;
+	}
 }
