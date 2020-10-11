cec6636d7caee9f5116c94efda75b8ae0971962c
Ashwin <ashwingairola999@gmail.com>
2020-10-11 18:50:19 +0530

    Removed the filter, instead added logic to unsubscribe.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,25 +3,27 @@ import {
 	Component,
 	ElementRef,
 	Inject,
+	OnDestroy,
 	OnInit,
 	Renderer2,
 	ViewChild
 } from '@angular/core';
-import { fromEvent, noop, Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { fromEvent, noop, Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 	@ViewChild('mobileNav') mobileNav!: ElementRef<HTMLDivElement>;
 	@ViewChild('desktopNav') desktopNav!: ElementRef<HTMLDivElement>;
 
 	showMobileMenu = false;
 	darkenNavbar = false;
 	scroll$: Observable<Event> = fromEvent(this.document, 'scroll');
+	unsub$: Subject<void> = new Subject();
 
 	constructor(
 		@Inject(DOCUMENT) private document: Document,
@@ -29,37 +31,38 @@ export class NavbarComponent implements OnInit {
 	) {}
 
 	ngOnInit(): void {
-		this.scroll$
-			.pipe(
-				filter((_: Event) => this.document.documentElement.scrollTop <= 100)
-			)
-			.subscribe(() => {
-				if (this.document.documentElement.scrollTop === 0) {
-					this.renderer.setStyle(
-						this.mobileNav.nativeElement,
-						'background-color',
-						'transparent'
-					);
+		this.scroll$.pipe(takeUntil(this.unsub$)).subscribe(() => {
+			if (this.document.documentElement.scrollTop === 0) {
+				this.renderer.setStyle(
+					this.mobileNav.nativeElement,
+					'background-color',
+					'transparent'
+				);
 
-					this.renderer.setStyle(
-						this.desktopNav.nativeElement,
-						'background-color',
-						'transparent'
-					);
-				} else {
-					this.renderer.setStyle(
-						this.mobileNav.nativeElement,
-						'background-color',
-						'rgba(0, 0, 0, 0.7)'
-					);
+				this.renderer.setStyle(
+					this.desktopNav.nativeElement,
+					'background-color',
+					'transparent'
+				);
+			} else {
+				this.renderer.setStyle(
+					this.mobileNav.nativeElement,
+					'background-color',
+					'rgba(0, 0, 0, 0.7)'
+				);
 
-					this.renderer.setStyle(
-						this.desktopNav.nativeElement,
-						'background-color',
-						'rgba(0, 0, 0, 0.7)'
-					);
-				}
-			}, noop);
+				this.renderer.setStyle(
+					this.desktopNav.nativeElement,
+					'background-color',
+					'rgba(0, 0, 0, 0.7)'
+				);
+			}
+		}, noop);
+	}
+
+	ngOnDestroy(): void {
+		this.unsub$.next();
+		this.unsub$.complete();
 	}
 
 	onClickBurger(): void {
