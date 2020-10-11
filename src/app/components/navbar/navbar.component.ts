9f1b3abf752509bc054957bfa4e8ef3298a02636
Ashwin <ashwingairola999@gmail.com>
2020-10-11 18:31:01 +0530

    Added logic to darken the navbar on scrolling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+	Component,
+	ElementRef,
+	Inject,
+	OnInit,
+	Renderer2,
+	ViewChild
+} from '@angular/core';
+import { fromEvent, noop, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-navbar',
@@ -6,11 +16,49 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+	@ViewChild('mobileNav') mobileNav!: ElementRef<HTMLDivElement>;
+	@ViewChild('desktopNav') desktopNav!: ElementRef<HTMLDivElement>;
+
 	showMobileMenu = false;
+	darkenNavbar = false;
+	scroll$: Observable<Event> = fromEvent(this.document, 'scroll');
 
-	constructor() {}
+	constructor(
+		@Inject(DOCUMENT) private document: Document,
+		private renderer: Renderer2
+	) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.scroll$
+			.pipe(filter((_: Event) => this.document.documentElement.scrollTop <= 10))
+			.subscribe(() => {
+				if (this.document.documentElement.scrollTop === 0) {
+					this.renderer.setStyle(
+						this.mobileNav.nativeElement,
+						'background-color',
+						'transparent'
+					);
+
+					this.renderer.setStyle(
+						this.desktopNav.nativeElement,
+						'background-color',
+						'transparent'
+					);
+				} else {
+					this.renderer.setStyle(
+						this.mobileNav.nativeElement,
+						'background-color',
+						'rgba(0, 0, 0, 0.7)'
+					);
+
+					this.renderer.setStyle(
+						this.desktopNav.nativeElement,
+						'background-color',
+						'rgba(0, 0, 0, 0.7)'
+					);
+				}
+			}, noop);
+	}
 
 	onClickBurger(): void {
 		this.showMobileMenu = true;
